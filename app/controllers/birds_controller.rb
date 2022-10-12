9b27914f2cbb67188251571f954f903fb31631f1
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-12 11:42:30 -0600

    okayyyy

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  validates :name, presence: true, uniqueness: true
 
   # GET /birds
   def index
@@ -46,5 +48,10 @@ class BirdsController < ApplicationController
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
   end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 
 end
